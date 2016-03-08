ad9c8a61b3c9c1f0b8a9837f6e21d444c6055915
Vincent Jousse <vincent@jousse.org>
2016-03-08 16:15:14 +0100

    Modify build to update to latest version

--- voxo-fast/build.sh
+++ voxo-fast/build.sh
@@ -1,3 +1,8 @@
-git clone git@bitbucket.org:vjousse/voxo-fast.git && \
+if [ ! -d "voxo-fast" ]; then
+    git clone git@bitbucket.org:vjousse/voxo-fast.git
+else
+    cd voxo-fast
+    git pull
+    cd ..
+fi
 docker build -t voxolab/voxo-fast .
-
