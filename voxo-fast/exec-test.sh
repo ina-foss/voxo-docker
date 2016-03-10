249e6f6ebcd45469a19056209b3fa2243a16b3b4
Vincent Jousse <vincent@jousse.org>
2016-03-10 12:27:41 +0100

    Update exec example

--- voxo-fast/exec-test.sh
+++ voxo-fast/exec-test.sh
@@ -1,1 +1,2 @@
-nvidia-docker exec voxo-fast /opt/voxo-fast/run.sh /mnt/audio/lcp_q_gov.wav /mnt/output/
+#nvidia-docker exec voxo-fast /opt/voxo-fast/run.sh /mnt/audio/lcp_q_gov.wav /mnt/output/
+nvidia-docker exec voxo-fast /opt/voxo-fast/run.sh /mnt/audio/lcp_q_gov.wav /mnt/output/ yes 8 /mnt/models/voxygen/3g.resumemo /mnt/models/voxygen/graphResumemo /mnt/models/modeleGlis
