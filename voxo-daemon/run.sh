d226da149baf7dd0bcebd870be3bb9d8008a7ba7
Vincent Jousse <vincent@jousse.org>
2016-03-16 21:09:58 +0100

    Add docker host as env param

--- voxo-daemon/run.sh
+++ voxo-daemon/run.sh
@@ -3,6 +3,4 @@ OUTPUT_DIR=/opt/asr/docker/voxo-docker/voxo-daemon/output
 CONF_FILE=/opt/asr/docker/voxo-docker/voxo-daemon/conf/daemon.conf
 DOCKER_NAME=voxo-daemon
 
-nvidia-docker run -ti --name $DOCKER_NAME -v $OUTPUT_DIR:/mnt -v $CONF_FILE:/opt/voxo-daemon/daemon.conf -v /opt/asr/models/:/mnt/models -v /opt/asr/output:/mnt/output -v /opt/asr/audio:/mnt/audio voxolab/daemon /opt/voxo-daemon/decode_daemon.py -n
-#docker run -dti --name $DOCKER_NAME -v $OUTPUT_DIR:/mnt v $CONF_FILE:/opt/voxo-daemon/daemon.conf voxolab/daemon
-#docker run -t -i --name $DOCKER_NAME -p 8000:8000 -u asr -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK -v $CONF_DIR:/mnt -v $CONF_DIR/config.py:/home/asr/voxo-dashboard/config.py -v $CONF_DIR/wsgi_prod.py:/home/asr/voxo-dashboard/wsgi_prod.py voxolab/dashboard
+nvidia-docker run -ti --name $DOCKER_NAME -v $OUTPUT_DIR:/mnt -v $CONF_FILE:/opt/voxo-daemon/daemon.conf -v /opt/asr/models/:/mnt/models -v /opt/asr/output:/mnt/output -e "DOCKER_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+'):8000" voxolab/daemon /opt/voxo-daemon/decode_daemon.py -n
