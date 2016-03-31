7d8919005cde9f72e4a745decfdc68da5c21497d
Vincent Jousse <vincent@jousse.org>
2016-03-31 16:43:15 +0200

    Compile lium gst-plugin version in kaldi-lium

--- kaldi/build.sh
+++ kaldi/build.sh
@@ -1,5 +1,14 @@
-git clone git@bitbucket.org:vjousse/kaldi.git && \
-cd kaldi && \
-git checkout lium-fast && \
-cd .. && \
+if [ ! -d "kaldi" ]; then
+
+    git clone git@bitbucket.org:vjousse/kaldi.git && \
+    cd kaldi && \
+    git checkout lium-fast-gst-plugin
+else
+    cd kaldi
+    git checkout lium-fast-gst-plugin
+    git pull
+fi
+
+cd ..
+
 docker build -t voxolab/kaldi-lium .
