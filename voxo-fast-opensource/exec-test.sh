9de3481e7f9ad90600220d47aaae0f257c1a4c2f
Vincent Jousse <vincent@jousse.org>
2016-07-15 17:56:29 +0200

    Fix dockerfile and scripts

--- voxo-fast-opensource/exec-test.sh
+++ voxo-fast-opensource/exec-test.sh
@@ -1,3 +1,1 @@
-#nvidia-docker exec voxo-fast /opt/voxo-fast/run.sh /mnt/audio/lcp_q_gov.wav /mnt/output/
-nvidia-docker exec voxo-fast /opt/voxo-fast/run.sh /mnt/audio/lcp_q_gov.wav /mnt/output/ yes 8 /mnt/models/voxygen/3g.resumemo /mnt/models/voxygen/graphResumemo /mnt/models/modeleGlis
-#nvidia-docker exec voxo-fast /opt/voxo-fast/run.sh /mnt/audio/photographer_secret_service.mp4 /mnt/output/ yes 8 /mnt/models/voxygen/en/nab/4g.fixed.nab /mnt/models/voxygen/en/nab/graph.nab /mnt/models/voxygen/en/model
+nvidia-docker exec voxo-fast-opensource /opt/voxo-fast-opensource/run.sh /mnt/audio/lcp_q_gov.wav /mnt/output/ yes 8 /mnt/models/voxygen/speech_constarpa/4g.constarpa.speech /mnt/models/voxygen/speech_constarpa/graph.speech /mnt/models/modeleGlis diarization.sh /mnt/models/voxygen/speech_constarpa/2g.speech
--- voxo-fast-opensource/run.sh
+++ voxo-fast-opensource/run.sh
@@ -1,1 +1,1 @@
-nvidia-docker run -dit --name voxo-fast -v /opt/asr/models/:/mnt/models -v /opt/asr/output:/mnt/output -v /opt/asr/audio:/mnt/audio voxolab/voxo-fast
+nvidia-docker run -dit --name voxo-fast-opensource -v /opt/asr/models/:/mnt/models -v /opt/asr/output:/mnt/output -v /opt/asr/audio:/mnt/audio voxolab/voxo-fast-opensource
