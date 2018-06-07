3bb2c27c10d93f306dbf6270f98bd87fd5d2c0e9
Vincent Jousse <vincent@jousse.org>
2018-06-07 16:08:36 +0200

    Update build

--- voxo-dashboard/build.sh
+++ voxo-dashboard/build.sh
@@ -1,17 +1,15 @@
 if [ ! -d "voxo-dashboard" ]; then
-    git clone git@bitbucket.org:vjousse/voxo-dashboard.git
+    git clone git@github.com:voxolab/voxo-dashboard.git
     cd voxo-dashboard
-    git checkout multi-model
 else
     cd voxo-dashboard
-    git checkout multi-model
     git pull
     cd ..
 fi
 
 
 if [ ! -d "voxo-lib" ]; then
-    git clone git@bitbucket.org:vjousse/voxo-lib.git
+    git clone git@github.com:voxolab/voxo-lib.git
 else
     cd voxo-lib
     git pull
@@ -19,5 +17,3 @@ else
 fi
 
 docker build -t voxolab/dashboard .
-
-docker save voxolab/dashboard > /opt/asr/docker/images/voxo-dashboard.docker
